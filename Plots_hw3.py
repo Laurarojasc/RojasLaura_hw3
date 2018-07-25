2e5816464daada2db280ff8a4f7e175ea8983d88
Laura Rojas Cardenas <40705332+Laurarojasc@users.noreply.github.com>
2018-07-24 21:39:49 -0500

    Update Plots_hw3.py

--- Plots_hw3.py
+++ Plots_hw3.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 
 #Graficas de la trayectoria de una particula en cargada en un campo magenetico dado
 
-#Leo los datos de RojasLaura_ODE.cpp, para esto genere un archivo datos.txt
+#Leo los datos de RojasLaura_ODE.cpp, para esto genere un archivo datos.dat
 
-x = np.genfromtxt("datos.txt",usecols=0)
-y = np.genfromtxt("datos.txt",usecols=1)
-z = np.genfromtxt("datos.txt",usecols=2)
-t = np.genfromtxt("datos.txt",usecols=3)
+x = np.genfromtxt("datos.dat",usecols=0)
+y = np.genfromtxt("datos.dat",usecols=1)
+z = np.genfromtxt("datos.dat",usecols=2)
+t = np.genfromtxt("datos.dat",usecols=3)
 
 #1. Graficas de x-t, x-z , y-t  
 
@@ -48,7 +48,7 @@ plt.savefig("posicion.png")
 
 # Leo los datos de RojasLaura_PDE.cpp
 
-datos = np.genfromtxt("init.txt")
+datos = np.genfromtxt("init.dat")
 
 # Generar los valores x,y
 x = np.linspace(0,1,100)  #crear 100 valores de x 
@@ -56,10 +56,11 @@ X,Y = np.meshgrid(x,x)  #valores de x y y ordenados
  
  
 #1. Grafica 3D de condiciones iniciales
+
 fig = plt.figure()
 ax = fig.gca(projection="3d")
 ax.plot_surface(X, Y, datos)
-plt.title("Condiciones iniciales (en t0)")
+plt.title("Condiciones iniciales")
 ax.set_xlabel("x")
 ax.set_ylabel("y")
 ax.set_zlabel("z")
@@ -69,15 +70,18 @@ fig.savefig("Condiciones_iniciales.png")
 
 #2. Grafica en 3D de la membrana en t=60ms, para esto genere un archivo de datos2.txt donde esten todos los tiempos y datos para el caso 1 y caso 2
 
-datos2 = np.genfromtxt("datos2.txt")
+datos2 = np.genfromtxt("datos2.dat")
 
 #Para extremos fijos
 
-filas = np.size(datos2,0)
+filas_size = np.size(datos2,0) #Aqui saco el tamano de los datos, la idea es que la mitad correspoden al caso 1 y la otra al caso 2 
 
-fijos = datos2[0:int(filas/2),:] #Meto todos los datos del caso 1 en una matriz 
 
-m = np.reshape(fijos[3600,:], (n,n)) #matematicamente clacule la linea que debo buscar donde estan los 60ms
+fijos = datos2[0:int(filas_size/2),:] #Meto todos los datos del caso 1 en un arreglo
+tiempo = fijos[:,0] #Escojo la primera columna del arrgelo que cree anteriormente, pues esta corresponde al tiempo
+fijos = fijos[:,1:] # No utilizo la columna del tiempo del arreglo fijos 
+
+m = np.reshape(fijos[5,:], (100,100)) #matematicamente clacule la linea que debo buscar donde estan los 60ms
 fig = plt.figure()
 ax = fig.gca(projection="3d")
 ax.plot_surface(X, Y, m)
@@ -88,20 +92,14 @@ ax.set_zlabel("z")
 fig.savefig("membrana_fijos_60ms.png")
 
 
-#Para extremos libres
+#Para extremos libres 
 
-libres = datos2[int(filas/2):,:] #Meto todos los datos del caso 2 en una matriz 
-
+libres = datos2[int(filas_size/2)+1:-1,:] #Meto todos los datos del caso 2 en un arreglo, cambia porque elijo los datos del caso 2 
 tiempo = libres[:,0] #La Primera columna es el tiempo
 libres = libres[:,1:] #Boto la columna del tiempo para libres 
-n = 100 # Tamano de datos en la membrana para reordenar los arreglos, pues deben ser del mismo tamano
- 
-# generar los valores x,y
-x = np.linspace(0,1,n)
-X,Y = np.meshgrid(x,x)
  
 # Grafica de condiciones iniciales
-m = np.reshape(libres[3600,:], (n,n)) #matematicamente clacule la linea que debo buscar donde estan los 60ms
+m = np.reshape(libres[5,:], (100,100)) #matematicamente clacule la linea que debo buscar donde estan los 60ms
 fig = plt.figure()
 ax = fig.gca(projection="3d")
 ax.plot_surface(X, Y, m)
@@ -111,74 +109,58 @@ ax.set_ylabel("y")
 ax.set_zlabel("z")
 fig.savefig("membrana_libres_60ms.png")
 
-
-
-
  
+# 3. Grafica de cortes transversales en x=L/2 cada 100 pasos de tiempo, para extremos fijos (Caso1)
 
-# 3. Grafica de cortes transversales en x=L/2 cada 10 pasos de tiempo, para extremos fijos (Caso1)
-
-pasos = 10 # Cada dos pasos de tiempo para este ejemplo
-col = fijos[0:-1:pt, :] # start:stop:step en las filas, selecciono todas las columnas
-n_pasos = np.size(col, 0)
+pasos = 100 # Cada 100 pasos 
+Columnas = fijos[0:-1:pasos, :] # Selecciono todas las columnas con: inicio:final:step en las filas
+pasos_n = np.size(Columnas, 0)
  
 fig = plt.figure()
 ax = fig.gca()
 
-for i in range(n_pasos):
-    x = np.reshape(col[i,:], (n,n)) # Seleccionar el instante de tiempo y reformar el arreglo
-    mitad = x[int(n/2),:] # Seleccionar la linea de la mitad
+for i in range(pasos_n):
+    x = np.reshape(Columnas[i,:], (100,100)) # Aqui elijo el tiempo y cambio el arreglo
+    mitad = x[int(100/2),:] # Seleccionar la linea de la mitad porque tengo todos los datos y asumo que la mitad corresponden al caso 1 
  
-    ax.plot(x, mitad, "o-")
+    ax.plot(x, mitad, ".-")
+    
 ax.grid()
 plt.xlabel("x")
-plt.ylabel("Amplitud")
+plt.ylabel("Pasos de tiempo")
 fig.savefig("membrana_corte_fijos.png")
 
 
 
 #Grafica de cortes transversales en x=L/2 cada 10 pasos de tiempo, para extremos libres (Caso2)
 
-pasos = 10 
-col = libres[0:-1:pt, :] # Selecciono todas las columnas, teniendo en cuenta que inicio:final:step en las filas
-n_pasos = np.size(col, 0)
+pasos = 100
+Columnas = libres[0:-1:pasos, :] # Selecciono todas las columnas con: inicio:final:step en las filas
+pasos_n = np.size(Columnas, 0)
  
 fig = plt.figure()
 ax = fig.gca()
-for i in range(n_pasos):
+for i in range(pasos_n):
             
-    x = np.reshape(col[i,:], (n,n)) # Aqui elijo el instante de tiempo y cambio el arreglo
-    mitad = x[int(n/2),:] # elegir la linea de la mitad
+    x = np.reshape(Columnas[i,:], (100,100)) # Aqui elijo el tiempo y cambio el arreglo
+    mitad = x[int(100/2),:] # elegir la linea de la mitad
  
-    ax.plot(x, mitad, "o-")
+    ax.plot(x, mitad, ".-")
 ax.grid()
 plt.xlabel("x")
-plt.ylabel("Amplitud")
+plt.ylabel("Pasos de tiempo")
 fig.savefig("membrana_corte_libres.png")
 
 
-# Animacion (Intento de Bono, si me funciono pero es lento)
-
-             
-#figura = plt.figure()
-#ax = fig.gca(projection="3d")
-            
-#tiempo = datos2[:,0]
 
 
-#def Animacion(i):
-#    ax.cla() # Limpiar el eje
-#    x = np.reshape(tiempo[i,:], (n,n)) # Reordenar los datos del instante de tiempo a graficar
-#    ax.plot_surface(X, Y, x)
- 
-#    plt.title("Membrana")
-#    ax.set_xlabel("x")
-#    ax.set_ylabel("y")
-#    ax.set_zlabel("z")
-#    ax.set_zlim([-1,1])
- 
-#animacion = FuncAnimation(fig, Animacion, frames = 10) #solo 10 para que no sea tan lento
-#anim.save("Animacion_membrana.gif', writer="imagemagick", fps=2)
+
+
+
+
+
+
+
 
 
 
